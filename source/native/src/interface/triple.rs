f14fac454c349c4851fb20a68eab6e637e5eadd3
Andrew Baxter <>
2026-05-09 20:09:11 +0900

    Cleanup

--- source/native/src/interface/triple.rs
+++ source/native/src/interface/triple.rs
@@ -3,13 +3,11 @@ use {
     shared::interface::triple::{
         FileHash,
         Node,
-        StrNode,
     },
     serde::{
         Serialize,
         Deserialize,
     },
-    std::str::FromStr,
 };
 
 #[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Serialize, Deserialize)]
@@ -17,13 +15,10 @@ pub struct DbNode(pub Node);
 
 impl GoodOrmningCustomString<DbNode> for DbNode {
     fn to_sql<'a>(value: &'a Self) -> String {
-        return StrNode(value.0.clone()).to_string();
+        return serde_json::to_string(&value.0).unwrap();
     }
 
     fn from_sql(value: String) -> Result<Self, String> {
-        if value.starts_with("f=") || value.starts_with("v=") {
-            return Ok(Self(StrNode::from_str(&value)?.0));
-        }
         return Ok(Self(serde_json::from_str(&value).map_err(|e| e.to_string())?));
     }
 }
@@ -33,13 +28,10 @@ pub struct DbFileHash(pub FileHash);
 
 impl GoodOrmningCustomString<DbFileHash> for DbFileHash {
     fn to_sql<'a>(value: &'a Self) -> String {
-        return value.0.to_string();
+        return serde_json::to_string(&value.0).unwrap();
     }
 
     fn from_sql(value: String) -> Result<Self, String> {
-        if value.contains(':') {
-            return Ok(Self(FileHash::from_str(&value)?));
-        }
         return Ok(Self(serde_json::from_str(&value).map_err(|e| e.to_string())?));
     }
 }
