a8ab146202e24f6bd18a34248d9f2faeda47e40f
Andrew Baxter <>
2026-05-17 00:18:58 +0900

    Extension cleanup

--- source/browser/ext_static/site_twitter_early.js
+++
@@ -1,7 +0,0 @@
-// Early content script (document_start, ISOLATED world) that injects the
-// fetch interceptor into the page context via a script src (avoids CSP inline block).
-{
-  const s = document.createElement('script');
-  s.src = /** @type {any} */ (globalThis).browser.runtime.getURL('site_twitter_intercept.js');
-  document.documentElement.prepend(s);
-}
